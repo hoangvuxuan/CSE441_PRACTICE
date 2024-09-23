6ffb088a14cd0eee129c3b42684a8acb9a86debb
xuan hoang <hoang24k69@gmail.com>
2024-09-23 16:27:49 +0700

    Ex02: backend

--- Ex02/app/src/main/java/com/example/ex02/MainActivity.java
+++ Ex02/app/src/main/java/com/example/ex02/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex02;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btncall;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "CR441 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, Subactivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
+
