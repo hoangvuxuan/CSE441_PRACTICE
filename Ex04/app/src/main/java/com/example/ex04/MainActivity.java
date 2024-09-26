27d0d2344817f3254a2097d97a57d94ae7ab39ca
xuan hoang <hoang24k69@gmail.com>
2024-09-26 19:56:44 +0700

    Ex04 convert F C

--- Ex04/app/src/main/java/com/example/ex04/MainActivity.java
+++ Ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtdoC, edtdoF;
+    Button btncf, btnfc, btn_clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        edtdoC = findViewById(R.id.edtdoC);
+        edtdoF = findViewById(R.id.edtdoF);
+        btncf = findViewById(R.id.btncf);
+        btnfc = findViewById(R.id.btnfc);
+        btn_clear = findViewById(R.id.btn_clear);
 
+        btncf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doC = edtdoC.getText() + "";
+
+                int C = Integer.parseInt(doC);
+                edtdoF.setText("" + dcf.format(C * 1.8 + 32));
+            }
+        });
+        btnfc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+
+                String doF = edtdoF.getText() + "";
+                int F = Integer.parseInt(doF);
+                edtdoC.setText("" + dcf.format((F - 32) / 1.8));
+            }
+        });
+
+        btn_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtdoC.setText("");
+                edtdoF.setText("");
+            }
+        });
     }
 }
