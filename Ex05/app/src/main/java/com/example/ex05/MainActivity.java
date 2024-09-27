294c1d7b8570477387603abc52715ae0d3b4272d
xuan hoang <hoang24k69@gmail.com>
2024-09-27 19:21:35 +0700

    Ex05: chuyen doi am duong - backend

--- Ex05/app/src/main/java/com/example/ex05/MainActivity.java
+++ Ex05/app/src/main/java/com/example/ex05/MainActivity.java
@@ -15,6 +15,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //public void onClick(View v) {
+
+        //String sa=edita.getText()+"";
+        //String sb=editb.getText()+"";
+        //String sc=editc.getText()+"";
+        //int a=Integer.parseInt(sa);
+        //int b=Integer.parseInt(sb);
+        //int c=Integer.parseInt(sc);
+        //String kq="";
+        //DecimalFormat dcf=new DecimalFormat("0.00");
+        //if(a==0)
+        //{
+        //if(b==0)
+        //{
+        //28
+        //if(c==0)
+        //kq="PT vô số nghiệm";
+        //else
+        //kq="PT vô nghiệm";
+        //}
+        //else
+        //{
+        //kq="Pt có 1 No, x="+dcf.format(-c/b);
+        //}
+        //}
+        //else
+        //{
+        //double delta=b*b-4*a*c;
+        //if(delta<0)
+        //{
+        //kq="PT vô nghiệm";
+        //}
+        //else if(delta==0)
+        //{
+        //kq="Pt có No kép x1=x2="+dcf.format(-b/(2*a));
+        //}
+        //else
+        //{
+        //kq="Pt có 2 No: x1="+dcf.format((-b+Math.sqrt(delta))/(2*a))+"; x2="+dcf.format((-
+        //b-Math.sqrt(delta))/(2*a));
+        //}
+        //}
+        //txtkq.setText(kq);
+        //}
+        //});
 
     }
 }
