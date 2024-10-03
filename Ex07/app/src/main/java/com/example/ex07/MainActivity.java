fef37bfe9fb7fb414c3763ab03f7c6a54caf9cda
xuan hoang <hoang24k69@gmail.com>
2024-10-03 13:27:23 +0700

    Ex07: navigation to bt2, bt3

--- Ex07/app/src/main/java/com/example/ex07/MainActivity.java
+++ Ex07/app/src/main/java/com/example/ex07/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex07;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_ex2;
+    Button btn_ex3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn_ex2.findViewById(R.id.btn_ex2);
+        btn_ex2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, bt_2_main.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_ex3.findViewById(R.id.btn_ex3);
+        btn_ex3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, bt_3_main.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
