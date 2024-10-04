b49c955d61d6b980fdfac9cbc17310f52b6ea3a6
xuan hoang <hoang24k69@gmail.com>
2024-10-04 14:36:15 +0700

    Ex08: backend

--- Ex08/app/src/main/java/com/example/ex08/MainActivity.java
+++ Ex08/app/src/main/java/com/example/ex08/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex08;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnCall = findViewById(R.id.btncallphone);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent1 =new Intent(MainActivity.this,CallPhoneActivity.class);
+                startActivity(intent1);
+            }
         });
+        Button btnSend = findViewById(R.id.btnsendsms);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 =new Intent(MainActivity.this,SendSMSActivity.class);
+                startActivity(intent2);
+            }
+        });
+
     }
 }
