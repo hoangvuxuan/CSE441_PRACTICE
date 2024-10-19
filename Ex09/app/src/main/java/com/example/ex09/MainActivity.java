ddbd72a16517e94a884c6ee9275003dfadb23188
xuan hoang <hoang24k69@gmail.com>
2024-10-19 13:33:14 +0700

    Ex09: done

--- Ex09/app/src/main/java/com/example/ex09/MainActivity.java
+++ Ex09/app/src/main/java/com/example/ex09/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex09;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPlay, btnStop;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnPlay = findViewById(R.id.btnPlay);
+        btnStop = findViewById(R.id.btnStop);
+
+        btnPlay.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MusicService.class);
+            intent.setAction("PLAY");
+            startService(intent);
         });
+
+        btnStop.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MusicService.class);
+            intent.setAction("STOP");
+            stopService(intent);
+            finish(); // Thoát ứng dụng
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    "MUSIC_CHANNEL",
+                    "Music Player",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(channel);
+            }
+        }
+
     }
 }
