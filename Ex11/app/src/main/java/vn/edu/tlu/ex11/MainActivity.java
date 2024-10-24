b4e4f3fa2c3c9898257e8ff296ca820e834f2f02
xuan hoang <hoang24k69@gmail.com>
2024-10-24 17:23:57 +0700

    Ex11 project 1

--- Ex11/app/src/main/java/vn/edu/tlu/ex11/MainActivity.java
+++ Ex11/app/src/main/java/vn/edu/tlu/ex11/MainActivity.java
@@ -1,20 +1,43 @@
 package vn.edu.tlu.ex11;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnOpen;
+    EditText edtLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        edtLink = findViewById(R.id.edtlink);
+        btnOpen = findViewById(R.id.btnopen);
+
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = edtLink.getText().toString().trim();
+                if (!url.isEmpty()) {
+                    // Chuyển đổi URL thành dạng hoàn chỉnh
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://" + url));
+                    intent.putExtra("url", url); // Truyền URL cho Activity thứ hai
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập URL", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
