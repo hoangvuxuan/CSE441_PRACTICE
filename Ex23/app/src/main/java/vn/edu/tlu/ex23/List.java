604063990652af77cd933d4c040b9148a25739b5
xuan hoang <hoang24k69@gmail.com>
2024-10-25 17:27:40 +0700

    Ex23 test data

---
+++ Ex23/app/src/main/java/vn/edu/tlu/ex23/List.java
@@ -0,0 +1,18 @@
+package vn.edu.tlu.ex23;
+
+import android.graphics.Bitmap;
+
+public class List {
+    private Bitmap img;
+    private String title, info, link;
+
+    public List(Bitmap img, String title, String info, String link) {
+        this.img = img;
+        this.title = title;
+        this.info = info;
+        this.link = link;
+    }
+
+   
+}
+
