ebb9faa8091a8e0f357ff674678751ca4a3ce4e8
xuan hoang <hoang24k69@gmail.com>
2024-10-25 11:10:51 +0700

    Ex18 main

--- Ex18/app/src/main/java/vn/edu/tlu/ex18/MainActivity.java
+++ Ex18/app/src/main/java/vn/edu/tlu/ex18/MainActivity.java
@@ -8,17 +8,143 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TabHost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    String DB_PATH_SUFFIX = "/databases/";
+    public static SQLiteDatabase database = null;
+    public static String DATABASE_NAME = "arirang.sqlite";
+    EditText edttim;
+    ListView lv1, lv2, lv3;
+    ArrayList<Item> list1, list2, list3;
+    MyArrayAdapter myarray1, myarray2, myarray3;
+    TabHost tab;
+    ImageButton btnxoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        processCopy(); // Copy CSDL arirang.sqlite
+
+        database = openOrCreateDatabase("arirang.sqlite", MODE_PRIVATE, null);
+        addControl(); // Thêm các Controls
+        addTim(); // Xử lý tìm kiếm
+        addEvents(); // Xử lý sự kiện
+    }
+
+    private void addControl() {
+        btnxoa = findViewById(R.id.btnxoa);
+        tab = findViewById(R.id.tabhost);
+        tab.setup();
+        TabHost.TabSpec tab1 = tab.newTabSpec("t1");
+        tab1.setContent(R.id.tab1);
+        tab1.setIndicator("", getResources().getDrawable(R.drawable.search));
+        tab.addTab(tab1);
+
+        TabHost.TabSpec tab2 = tab.newTabSpec("t2");
+        tab2.setContent(R.id.tab2);
+        tab2.setIndicator("", getResources().getDrawable(R.drawable.list));
+        tab.addTab(tab2);
+
+        TabHost.TabSpec tab3 = tab.newTabSpec("t3");
+        tab3.setContent(R.id.tab3);
+        tab3.setIndicator("", getResources().getDrawable(R.drawable.favourite));
+        tab.addTab(tab3);
+
+        edttim = findViewById(R.id.edttim);
+        lv1 = findViewById(R.id.listview1);
+        lv2 = findViewById(R.id.listview2);
+        lv3 = findViewById(R.id.listview3);
+        list1 = new ArrayList<Item>();
+        list2 = new ArrayList<Item>();
+        list3 = new ArrayList<Item>();
+        loadData(); // Load dữ liệu
+    }
+
+    private void addTim() {
+        edttim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchText = edttim.getText().toString();
+                list1.clear();
+                Cursor c = database.rawQuery("SELECT * FROM ArirangSongList WHERE TIEUDE LIKE'%" + searchText + "%'", null);
+                while (c.moveToNext()) {
+                    Item item = new Item();
+                    item.setMaso(c.getString(0));
+                    item.setTieude(c.getString(2));
+                    item.setThich(c.getInt(6));
+                    list1.add(item);
+                }
+                myarray1.notifyDataSetChanged();
+                c.close();
+            }
         });
+    }
+
+    private void addEvents() {
+        btnxoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list3.clear();
+                Cursor c = database.rawQuery("SELECT * FROM ArirangSongList WHERE YEUTHICH=1", null);
+                while (c.moveToNext()) {
+                    Item item = new Item();
+                    item.setMaso(c.getString(0));
+                    item.setTieude(c.getString(2));
+                    item.setThich(c.getInt(6));
+                    list3.add(item);
+                }
+                myarray3.notifyDataSetChanged();
+                c.close();
+            }
+        });
+    }
+
+    private void loadData() {
+        Cursor c = database.rawQuery("SELECT * FROM ArirangSongList", null);
+        while (c.moveToNext()) {
+            Item item = new Item();
+            item.setMaso(c.getString(0));
+            item.setTieude(c.getString(2));
+            item.setThich(c.getInt(6));
+            list2.add(item);
+        }
+        myarray2 = new MyArrayAdapter(this, R.layout.item_list, list2);
+        lv2.setAdapter(myarray2);
+        c.close();
+    }
+
+    private void processCopy() {
+        String dbPath = getApplicationInfo().dataDir + DB_PATH_SUFFIX + DATABASE_NAME;
+        File file = new File(dbPath);
+        if (!file.exists()) {
+            try {
+                InputStream input = getResources().openRawResource(R.raw.arirang);
+                OutputStream output = new FileOutputStream(dbPath);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = input.read(buffer)) > 0) {
+                    output.write(buffer, 0, length);
+                }
+                output.flush();
+                output.close();
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ Ex18/app/src/main/java/vn/edu/tlu/ex18/MyArrayAdapter.java
@@ -0,0 +1,87 @@
+package vn.edu.tlu.ex18;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyArrayAdapter extends ArrayAdapter<Item> {
+    Activity context;
+    ArrayList<Item> myArray;
+    int layoutId;
+
+    public MyArrayAdapter(Activity context, int layoutId, ArrayList<Item> arr) {
+        super(context, layoutId, arr);
+        this.context = context;
+        this.layoutId = layoutId;
+        this.myArray = arr;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        convertView = inflater.inflate(layoutId, null);
+        final Item myItem = myArray.get(position);
+        final TextView tieude = convertView.findViewById(R.id.txttieude);
+        tieude.setText(myItem.getTieude());
+        final TextView maso = convertView.findViewById(R.id.txtmaso);
+        maso.setText(myItem.getMaso());
+        final ImageView btnlike = convertView.findViewById(R.id.btnlike);
+        final ImageView btnunlike = convertView.findViewById(R.id.btnunlike);
+        int thich = myItem.getThich();
+
+        if (thich == 0) {
+            btnlike.setVisibility(View.INVISIBLE);
+            btnunlike.setVisibility(View.VISIBLE);
+        } else {
+            btnunlike.setVisibility(View.INVISIBLE);
+            btnlike.setVisibility(View.VISIBLE);
+        }
+
+        btnlike.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                ContentValues values = new ContentValues();
+                values.put("YEUTHICH", 0);
+                MainActivity.database.update("ArirangSongList", values,
+                        "MABH=?", new String[]{maso.getText().toString()});
+                btnlike.setVisibility(View.INVISIBLE);
+                btnunlike.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnunlike.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                ContentValues values = new ContentValues();
+                values.put("YEUTHICH", 1);
+                MainActivity.database.update("ArirangSongList", values,
+                        "MABH=?", new String[]{maso.getText().toString()});
+                btnunlike.setVisibility(View.INVISIBLE);
+                btnlike.setVisibility(View.VISIBLE);
+            }
+        });
+
+        tieude.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tieude.setTextColor(Color.RED);
+                maso.setTextColor(Color.RED);
+                Intent intent1 = new Intent(context, ActivitySub.class);
+                Bundle bundle1 = new Bundle();
+                bundle1.putString("maso", maso.getText().toString());
+                intent1.putExtra("package", bundle1);
+                context.startActivity(intent1);
+            }
+        });
+
+        return convertView;
+    }
+}
+
