5f9eb05875005da9e6f35ab18822fce8ee04b08d
xuan hoang <hoang24k69@gmail.com>
2024-10-25 16:53:57 +0700

    Ex22 test 1

--- Ex22/app/build.gradle.kts
+++ Ex22/app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("org.apache.httpcomponents:httpclient:4.5.13")
 }
