0fa5f56e37bce10ee27c7ec8344ca43e5c9bef5d
xuan hoang <hoang24k69@gmail.com>
2024-10-25 10:33:29 +0700

    Ex17 done

--- Ex17/app/src/main/java/vn/edu/tlu/ex17/MainActivity.java
+++ Ex17/app/src/main/java/vn/edu/tlu/ex17/MainActivity.java
@@ -1,3 +1,18 @@
+package vn.edu.tlu.ex17;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 public class MainActivity extends AppCompatActivity {
     EditText edta, edtb, edtkq;
     Button btntong, btnclear;
