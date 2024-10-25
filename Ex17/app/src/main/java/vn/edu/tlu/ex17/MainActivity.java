d8967d00c9a77c0b14fc34810cb664209779e341
xuan hoang <hoang24k69@gmail.com>
2024-10-25 10:30:43 +0700

    Ex17 run 1

--- Ex17/app/src/main/java/vn/edu/tlu/ex17/MainActivity.java
+++ Ex17/app/src/main/java/vn/edu/tlu/ex17/MainActivity.java
@@ -1,24 +1,63 @@
-package vn.edu.tlu.ex17;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
+    EditText edta, edtb, edtkq;
+    Button btntong, btnclear;
+    TextView txtlichsu;
+    String lichsu = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Khai báo các biến giao diện
+        edta = findViewById(R.id.edta);
+        edtb = findViewById(R.id.edtb);
+        edtkq = findViewById(R.id.edtkq);
+        btntong = findViewById(R.id.btntong);
+        btnclear = findViewById(R.id.btnclear);
+        txtlichsu = findViewById(R.id.txtlichsu);
+
+        // Lấy lại dữ liệu trong SharedPreferences
+        SharedPreferences myprefs = getSharedPreferences("mysave", MODE_PRIVATE);
+        lichsu = myprefs.getString("ls", "");
+        txtlichsu.setText(lichsu);
+
+        // Xử lý sự kiện tính tổng
+        btntong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy giá trị từ EditText
+                int a = Integer.parseInt(edta.getText().toString());
+                int b = Integer.parseInt(edtb.getText().toString());
+                int kq = a + b;
+
+                // Hiển thị kết quả
+                edtkq.setText(String.valueOf(kq));
+
+                // Cập nhật lịch sử
+                lichsu += a + " + " + b + " = " + kq + "\n";
+                txtlichsu.setText(lichsu);
+            }
         });
+
+        // Xử lý sự kiện xóa lịch sử
+        btnclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lichsu = "";
+                txtlichsu.setText(lichsu);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Lưu lịch sử vào SharedPreferences
+        SharedPreferences myprefs = getSharedPreferences("mysave", MODE_PRIVATE);
+        SharedPreferences.Editor myedit = myprefs.edit();
+        myedit.putString("ls", lichsu);
+        myedit.commit();
     }
 }
