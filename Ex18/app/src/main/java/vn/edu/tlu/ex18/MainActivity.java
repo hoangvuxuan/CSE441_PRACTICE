88a3ab1689469227e396d5c5b4a024f08e5f89ca
xuan hoang <hoang24k69@gmail.com>
2024-10-25 11:17:10 +0700

    Ex18 run

--- Ex18/app/src/main/java/vn/edu/tlu/ex18/MainActivity.java
+++ Ex18/app/src/main/java/vn/edu/tlu/ex18/MainActivity.java
@@ -20,6 +20,11 @@ import android.widget.TabHost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
